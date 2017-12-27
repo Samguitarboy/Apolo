00a8accb9660192dc79c46a1a92af4ba7bc0dce2
s1031416 <s091139@gmail.com>
2017-12-27 21:29:54 +0800

    node moving test

--- src/main/java/com/apolo/controllers/GamepageController.java
+++ src/main/java/com/apolo/controllers/GamepageController.java
@@ -4,7 +4,11 @@ import com.apolo.MySQLConnector;
 import com.apolo.config;
 import javafx.fxml.FXML;
 import com.gluonhq.particle.view.ViewManager;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.scene.control.Accordion;
 import javafx.scene.control.Button;
 import javafx.scene.control.TitledPane;
@@ -19,20 +23,46 @@ public class GamepageController {
 
     @FXML
     private Button main1;
-
+    @FXML
+    private Circle node1,node2;
     @FXML
     private Accordion accordion;
-    public void initialize(){
+
+    public void initialize() {
         // TODO
 
     }
-    public void postInit() {
-        main1.setOnAction(e -> viewManager.switchView("main"));
 
+    public void postInit() {
+        main1.setOnAction(e -> {
+
+            Timer timer = new Timer(); //建立一個Timer物件
+            TimerTask nodemove = new TimerTask() {//也可以用匿名類別的方式，
+            int count = -420;
+            int temp;
+
+                @Override
+                public void run() {
+                    // TODO Auto-generated method stub
+                    node1.setTranslateY(count);//輸出時間
+                    
+                    if (count<350)
+                        temp = count-50;
+                    
+                    node2.setTranslateY(temp);
+                    count += 1;
+                    if(count == 350)
+                        timer.cancel();
+                }
+            };
+            timer.schedule(nodemove, 0, 2);
+            // TimeUnit.MICROSECONDS.sleep(100);
+        });
+        // node1.setCenterY(100);
     }
 
     public void dispose() {
 
     }
-    
+
 }
