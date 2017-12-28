f7773c6495db2cc3f8bda84742fcb6901d15c42f
s1031416 <s091139@gmail.com>
2017-12-28 21:56:43 +0800

    keypress detect (undone)

--- src/main/java/com/apolo/controllers/GamepageController.java
+++ src/main/java/com/apolo/controllers/GamepageController.java
@@ -1,18 +1,17 @@
 package com.apolo.controllers;
 
-import com.apolo.MySQLConnector;
-import com.apolo.config;
 import javafx.fxml.FXML;
 import com.gluonhq.particle.view.ViewManager;
+import java.awt.event.KeyEvent;
 import java.util.Timer;
 import java.util.TimerTask;
-import java.util.concurrent.TimeUnit;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
 import javafx.scene.control.Accordion;
 import javafx.scene.control.Button;
-import javafx.scene.control.TitledPane;
-import javafx.scene.layout.GridPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
 import javafx.scene.shape.Circle;
 import javax.inject.Inject;
 
@@ -20,45 +19,45 @@ public class GamepageController {
 
     @Inject
     private ViewManager viewManager;
-
     @FXML
     private Button main1;
     @FXML
-    private Circle node1,node2;
+    private Circle node1, node2;
     @FXML
-    private Accordion accordion;
+    private StackPane scene;
 
     public void initialize() {
         // TODO
-
+        scene.setOnKeyPressed((Event event) -> {
+            System.out.println(event.toString().substring(181));
+        });
     }
 
     public void postInit() {
         main1.setOnAction(e -> {
-
-            Timer timer = new Timer(); //建立一個Timer物件
-            TimerTask nodemove = new TimerTask() {//也可以用匿名類別的方式，
+            viewManager.switchView("main");
+        });
+        Timer timer = new Timer();
+        TimerTask nodemove = new TimerTask() {
             int count = -420;
             int temp;
 
-                @Override
-                public void run() {
-                    // TODO Auto-generated method stub
-                    node1.setTranslateY(count);//輸出時間
-                    
-                    if (count<350)
-                        temp = count-50;
-                    
-                    node2.setTranslateY(temp);
-                    count += 1;
-                    if(count == 350)
-                        timer.cancel();
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub   
+                node1.setTranslateY(count);
+                if (count < 350) {
+                    temp = count - 50;
                 }
-            };
-            timer.schedule(nodemove, 0, 2);
-            // TimeUnit.MICROSECONDS.sleep(100);
-        });
-        // node1.setCenterY(100);
+                node2.setTranslateY(temp);
+                count += 1;
+                if (count == 350) {
+                    timer.cancel();
+                }
+            }
+        };
+        timer.schedule(nodemove, 0, 2);
+
     }
 
     public void dispose() {
